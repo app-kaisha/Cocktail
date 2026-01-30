9d86f75fffd252d39512e47710b862b9f6659489
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-30 21:20:24 +0000

    Initial Commit

---
+++ Cocktail/CocktailApp.swift
@@ -0,0 +1,18 @@
+//
+//  CocktailApp.swift
+//  Cocktail
+//
+//  Created by app-kaihatsusha on 30/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CocktailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
